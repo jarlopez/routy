67b2c9c8777d654d2cf0a72eea690971d36e9ce7
Johan Mickos <jmickos@bu.edu>
2016-09-16 14:14:27 +0200

    Finalize link-state protocol in routy

--- routy.erl
+++ routy.erl
@@ -6,6 +6,8 @@
         ,init/1
         ,router/6
         ,send_status/1
+        ,bootstrap/0
+        ,cleanup/0
         ]).
 
 start(Reg, Name) ->
@@ -14,6 +16,26 @@ start(Reg, Name) ->
 stop(Node) ->
     Node ! stop,
     unregister(Node).
+
+bootstrap() ->
+    Ip = 'sweden@2001:6b0:1:1041:e888:f9d6:6ce6:bf413',
+    start(r1, stockholm),
+    start(r2, lund),
+    start(r3, malmo),
+
+    r1 ! {add, lund, {r2, Ip}},
+    % NOTE: Need bidirectional communication to reveal to Stockholm that Lund can connect to Malmo
+    r2 ! {add, stockholm, {r1, Ip}},
+    r2 ! {add, malmo, {r3, Ip}},
+    r2 ! broadcast,
+    r1 ! broadcast,
+    r3 ! broadcast.
+
+cleanup() ->
+    r1 ! stop,
+    r2 ! stop,
+    r3 ! stop.
+
 
 init(Name) ->
     Intf = intf:new(),
@@ -30,10 +52,10 @@ send_status(Pid) ->
     end.
 
 router(Name, N, Hist, Intf, Table, Map) ->
-    io:format("Running as ~p", [Name]),
     receive
         {add, Node, Pid} ->
-            io:format("[ADD @ ~p] ~p at ~p", [Name, Node, Pid]),
+            % TODO Shouldn't add also add to Map?
+            io:format("[ADD @ ~p] ~p at ~p~n", [Name, Node, Pid]),
             Ref = erlang:monitor(process,Pid),
             Intf1 = intf:add(Node, Ref, Pid, Intf),
             router(Name, N, Hist, Intf1, Table, Map);
@@ -50,6 +72,46 @@ router(Name, N, Hist, Intf, Table, Map) ->
         {status, From} ->
             From ! {status, {Name, N, Hist, Intf, Table, Map}},
             router(Name, N, Hist, Intf, Table, Map);
+        status ->
+            io:format("[~p]~nN: ~p~nHist:~n~p~n~nIntf list: ~p~nTable:~n~p~n~nMap:~n~p~n~n", [Name, N, Hist, intf:list(Intf), Table, Map]),
+            router(Name, N, Hist, Intf, Table, Map);
+        {links, Node, R, Links} ->
+            io:format("[links @ ~p] ~p - ~p - ~p ~n", [Name, Node, R, Links]),
+            case hist:update(Node, R, Hist) of
+                {new, Hist1} ->
+                    intf:broadcast({links, Node, R, Links}, Intf),
+                    Map1 = map:update(Node, Links, Map),
+                    router(Name, N, Hist1, Intf, Table, Map1);
+                old ->
+                    router(Name, N, Hist, Intf, Table, Map)
+            end;
+        {route, Name, From, Message} ->
+            io:format("~w: received message ~p from ~w~n", [Name, Message, From]),
+            router(Name, N, Hist, Intf, Table, Map);
+        {route, To, From, Message} ->
+            io:format("~w: routing message (~p)", [Name, Message]),
+            case dijkstra:route(To, Table) of
+                {ok, Gw} ->
+                    case intf:lookup(Gw, Intf) of
+                        {ok, Pid} ->
+                            Pid ! {route, To, From, Message};
+                        not_found ->
+                            ok
+                    end;
+                not_found ->
+                    ok % :(
+            end,
+            router(Name, N, Hist, Intf, Table, Map);
+        {send, To, Message} ->
+            self() ! {route, To, Name, Message},
+            router(Name, N, Hist, Intf, Table, Map);
+        update ->
+            Table1 = dijkstra:table(intf:list(Intf), Map),
+            router(Name, N, Hist, Intf, Table1, Map);
+        broadcast ->
+            Message = {links, Name, N, intf:list(Intf)},
+            intf:broadcast(Message, Intf),
+            router(Name, N+1, Hist, Intf, Table, Map);
         stop ->
             ok
     end.
