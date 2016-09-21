b895e5c15fe07128e597b8de95d4281ae96d6dec
Johan Mickos <jmickos@bu.edu>
2016-09-21 16:49:32 +0200

    Clean up routy, add color messages

--- routy.erl
+++ routy.erl
@@ -32,20 +32,22 @@ send_status(Pid) ->
 router(Name, N, Hist, Intf, Table, Map) ->
     receive
         {add, Node, Pid} ->
-            io:format("[ADD @ ~p] ~p at ~p~n", [Name, Node, Pid]),
+            io:format("[~s]\t~-13w~13w\t~w~n", [color:cyan("add"), Name, Node, Pid]),
             Ref = erlang:monitor(process,Pid),
             Intf1 = intf:add(Node, Ref, Pid, Intf),
             Message = {links, Name, N, intf:list(Intf1)},
             intf:broadcast(Message, Intf1),
             router(Name, N+1, Hist, Intf1, Table, Map);
+
         {remove, Node} ->
             {ok, Ref} = intf:ref(Node, Intf),
             erlang:demonitor(Ref),
             Intf1 = intf:remove(Node, Intf),
             router(Name, N, Hist, Intf1, Table, Map);
+
         {'DOWN', Ref, process, _, _} ->
             {ok, Down} = intf:name(Ref, Intf),
-            io:format("~w: exit recived from ~w~n", [Name, Down]),
+            io:format("[~s]\t~-13w\tExit recived from ~w~n", [color:redb("down"), Name, Down]),
             Intf1 = intf:remove(Down, Intf),
 
             Map1 = map:update(Down, [], Map),
@@ -57,31 +59,21 @@ router(Name, N, Hist, Intf, Table, Map) ->
             Message = {links, Name, N, intf:list(Intf1)},
             intf:broadcast(Message, Intf1),
 
-
-            % Update Map, Table
             router(Name, N+1, Hist1, Intf1, Table1, Map1);
 
-%             % Update table with removed interface/node
-%             Table1 = dijkstra:table(intf:list(Intf1), Map),
-
-%             % Broadcast change to alert self, network
-%             Message = {links, Name, N, intf:list(Intf1)},
-%             intf:broadcast(Message, Intf1),
-
-%             router(Name, N + 1, Hist, Intf1, Table1, Map);
-
-% >>>>>>> Stashed changes
         {status, From} ->
             io:format("Received status from: ~p~n", [From]),
             From ! {status, {Name, N, Hist, Intf, Table, Map}},
             router(Name, N, Hist, Intf, Table, Map);
+
         status ->
             io:format("[~p]~nN: ~p~nHist:~n~p~n~nIntf list: ~p~nTable:~n~p~n~nMap:~n~p~n~n", [Name, N, Hist, intf:list(Intf), Table, Map]),
             router(Name, N, Hist, Intf, Table, Map);
+
         {links, Node, R, Links} ->
-            io:format("[links @ ~p] ~p - ~p - ~p ~n", [Name, Node, R, Links]),
             case hist:update(Node, R, Hist) of
                 {new, Hist1} ->
+                    io:format("[~s]\t~-13w~13w\tN=~w\t~w ~n", [color:magenta("link"), Name, Node, R, Links]),
                     intf:broadcast({links, Node, R, Links}, Intf),
                     Map1 = map:update(Node, Links, Map),
                     Table1 = dijkstra:table(intf:list(Intf), Map1),
@@ -89,11 +81,12 @@ router(Name, N, Hist, Intf, Table, Map) ->
                 old ->
                     router(Name, N, Hist, Intf, Table, Map)
             end;
+
         {route, Name, From, Message} ->
-            io:format("~w: received message ~p from ~w~n", [Name, Message, From]),
+            io:format("[~s]\t~-13s Received message ~s from ~s~n", [color:greenb("route"), Name, color:greenb(Message), From]),
             router(Name, N, Hist, Intf, Table, Map);
         {route, To, From, Message} ->
-            io:format("~w: Routing from ~p to ~p~n", [Name, From, To]),
+            io:format("[~s]\t~-13w Routing from ~p to ~p~n", [color:yellow("route"), Name, From, To]),
             case dijkstra:route(To, Table) of
                 {ok, Gw} ->
                     case intf:lookup(Gw, Intf) of
@@ -103,21 +96,19 @@ router(Name, N, Hist, Intf, Table, Map) ->
                             ok
                     end;
                 notfound ->
-                    io:format("Dropping packet from ~p to ~p~n", [From, To]),
+                    io:format("[~s]\tDropping packet from ~p to ~p~n", [color:redb("drop"), From, To]),
                     ok % :(
             end,
             router(Name, N, Hist, Intf, Table, Map);
+
         {send, To, Message} ->
             self() ! {route, To, Name, Message},
             router(Name, N, Hist, Intf, Table, Map);
+
         update ->
             Table1 = dijkstra:table(intf:list(Intf), Map),
             router(Name, N, Hist, Intf, Table1, Map);
         broadcast ->
-% <<<<<<< Updated upstream
-% =======
-%             io:format("Broadcasting from ~p~n", [Name]),
-% >>>>>>> Stashed changes
             Message = {links, Name, N, intf:list(Intf)},
             intf:broadcast(Message, Intf),
             router(Name, N+1, Hist, Intf, Table, Map);
