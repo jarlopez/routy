2286f6177279ee59aeca0416b281c5c40abcc10a
Johan Mickos <jmickos@bu.edu>
2016-09-21 13:36:55 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-erl -name $1@2001:6b0:1:1041:e888:f9d6:6ce6:bf413 -setcookie routy -connect_all false
+HOST='j@2001:6b0:1:1041:e888:f9d6:6ce6:bf413'
+if [ ! -z "$1" ]; then
+    HOST="${$1}@${$2}"
+fi
+
+erl -name ${HOST} -setcookie hemlis -connect_all false
